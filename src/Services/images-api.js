a18bf9c6747047790587a50a08d3d3dfd802abfa
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-12-21 15:06:20 +0200

    fix axios params

--- src/Services/images-api.js
+++ src/Services/images-api.js
@@ -1,21 +1,19 @@
 import axios from 'axios';
 
-const KEY = '18396149-73dfa4d2cc3cf60487110b893';
-const baseURL =
-  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12';
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.params = {
+  per_page: 12,
+  key: '18396149-73dfa4d2cc3cf60487110b893',
+  image_type: 'photo',
+  orientation: 'horizontal',
+};
 
 async function fetchImages(searchImage = '', page = 1) {
   try {
-    const response = await axios.get(baseURL, {
-      params: {
-        page: page,
-        q: searchImage,
-        key: KEY,
-      },
-    });
+    const response = await axios.get(`?page =${page}&q =${searchImage}`);
     return response.data;
   } catch (error) {
-    console.error(error);
+    return [];
   }
 }
 
