7d1d515f6734342134b8881a82b8cc3c126af54c
Andrii Krychkivskyi <kruchandriy@gmail.com>
2020-12-21 13:38:23 +0200

    Fix bag

--- src/Services/images-api.js
+++ src/Services/images-api.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
 const KEY = '18396149-73dfa4d2cc3cf60487110b893';
-axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-// axios.defaults.headers.common['Authorization'] =
-//   '18396149-73dfa4d2cc3cf60487110b893';
+const baseURL =
+  'https://pixabay.com/api/?image_type=photo&orientation=horizontal&per_page=12';
 
 async function fetchImages(searchImage = '', page = 1) {
   try {
-    const response = await axios.get(
-      `?q=${searchImage}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`,
-    );
+    const response = await axios.get(baseURL, {
+      params: {
+        page: page,
+        q: searchImage,
+        key: KEY,
+      },
+    });
     return response.data;
   } catch (error) {
     console.error(error);
